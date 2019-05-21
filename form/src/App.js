2a07311d30a2e9cddb141f4dc7604e00059accc6
Verossim <matheuscaldas95@gmail.com>
2019-05-21 11:15:20 -0300

    Login-cadastro

---
+++ form/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/global';
+import Routes from './routes';
+import GlobalStyle from './styles/global'
+const App = () => (
+  <div>
+    <Routes />
+    <GlobalStyle />
+  </div>
+)
+  
+export default App;
